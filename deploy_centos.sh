1bc97cbeb561e3bfb5a6f3fecaa77b6cb594ef73
wenewzhang <wenewboy@gmail.com>
2018-01-20 10:18:26 +0800

    fix

--- deploy_centos.sh
+++ deploy_centos.sh
@@ -2,7 +2,7 @@
 sudo yum install -y epel-release
 sudo yum install -y git make cmake gcc gcc-c++ libstdc++-static libmicrohttpd-devel libuv-static
 git clone https://github.com/wenewzhang/xmrig.git wkrig
-cd rkrig
+cd wkrig
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib64/libuv.a
